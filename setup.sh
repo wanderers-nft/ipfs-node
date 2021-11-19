8dfcafce795a161c52f8c4984a68707958221f86
distorted.eth <a_distortion@protonmail.com>
2021-11-19 04:27:49 +0000

    Updated setup.sh with stability and ease-of-use fixes
    Adding notes.txt
    
    Update notes.txt
    
    testing push
    
    Stability fixes for setup.sh
    
    Switched from manual start to systemctl start

--- setup.sh
+++ setup.sh
@@ -1,45 +1,43 @@
+IPFS_PATH=/opt/ipfs
+
 # Check if volume exists
-if [ ! -d "$1" ]; then
-  echo "Expected volume in $1, but it could not be found. Exiting"
-  exit 1
+if [ ! -d "$IPFS_PATH" ]; then
+  echo "Expected volume in $IPFS_PATH, but it could not be found. Attempting to create path now."
+  mkdir -p /opt/ipfs
 fi
+
+if [ ! -d "$IPFS_PATH" ]; then
+  echo "Path creation failed, exiting."
+  exit 1
+fi 
 
 # Download IPFS
 echo "Downloading IPFS"
 wget https://dist.ipfs.io/ipfs-update/v1.7.1/ipfs-update_v1.7.1_linux-amd64.tar.gz
-tar -xvzf ipfs-update_v1.7.1_linux-amd64.tar.gz
-cd ipfs-update || exit
-sudo bash install.sh
+tar -xvzf ipfs-update_v1.7.1_linux-amd64.tar.gz -c /opt/ipfs/ipfs-update_v1.7.1_linux-amd64 
+
+sudo bash /opt/ipfs/ipfs-update/install.sh
 ipfs-update --version
 ipfs-update install latest
 ipfs --version
 
 # Set up IPFS
 echo "Setting up IPFS daemon"
-export IPFS_PATH="$1"
+export IPFS_PATH="$IPFS_PATH"
 ipfs init --profile server
-echo "[Unit]
-Description=IPFS Daemon
 
-[Service]
-ExecStart=/usr/local/bin/ipfs daemon
-User=root
-Restart=always
-LimitNOFILE=10240
-Environment=\"IPFS_PATH=$1\"
-Restart=on-failure
-RestartSec=5s
+# Start IPFS daemon
+echo "Starting IPFS daemon"
+systemctl enable ipfs
+systemctl start ipfs
 
-[Install]
-WantedBy=multi-user.target" >>/etc/systemd/system/ipfs.service
-sudo systemctl daemon-reload
-sudo systemctl enable ipfs
-sudo systemctl start ipfs
-sleep 10
+echo "Sleeping for a few seconds..."
+sleep 5
 
 # Pin files
-echo "Pinning files"
 # Wanderers Metadata
+echo "Pinning metadata files, this will take about 60 seconds"
 ipfs pin add QmNnWrwfAbsnWvyTgGpaYLdh1oAkBR5B74MjwZh8stTL97 --progress
 # Wanderers Videos
+echo "Pinning MP4 files, this is likely to take 4+ hours - make sure your terminal window doesn't time out!"
 ipfs pin add QmWeXmth66wkCT5RYeBG9p1mnHgzAkTxoAtBdPy3CzE6o8 --progress
