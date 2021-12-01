60632ceecbb8cf2a324780cec26f09cb248e7e52
Anson <git@anson-cheung.com>
2021-12-02 07:09:12 +0800

    Update for WS16 Posters

--- start.sh
+++ start.sh
@@ -7,7 +7,11 @@ sleep 1
 # Files (in order):
 # The Wanderers (metadata)
 # The Wanderers
+# WS16 Posters (metadata)
+# WS16 Posters
 docker-compose exec -dT ipfs ipfs pin add \
 QmNnWrwfAbsnWvyTgGpaYLdh1oAkBR5B74MjwZh8stTL97 \
 QmWeXmth66wkCT5RYeBG9p1mnHgzAkTxoAtBdPy3CzE6o8 \
+QmY4GnpgS3eF95qShPDGr7zLXwffBbybF2aeY4mqoEZcCm \
+Qmbj1YhaGYHAdMBrVum7BAJP5ECJuarLcYaRFdEzDEBFN2 \
 --progress
